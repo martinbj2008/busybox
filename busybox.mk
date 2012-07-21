c9cbb309798c6cfe7d0a6da34347430cf3efea2c
Martin Zhang <martinbj2008@gmail.com>
2012-07-21 19:35:50 +0800

    busybox fix
    Signed-off-by: Martin Zhang <martinbj2008@gmail.com>

--- busybox.mk
+++ busybox.mk
@@ -1,5 +1,11 @@
+-include ../../env.mk
+
 all:
 	make
 prepare:
 	#busybox need not configuration. 
+	@sed 's:CONFIG_PREFIX="":CONFIG_PREFIX="${ROOT_ROMFS}":g' busybox.config>.config
+
+install:
+	@cp -v busybox ${ROOT_ROMFS}/bin
 
